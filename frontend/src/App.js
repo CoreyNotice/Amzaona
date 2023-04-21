d25cc40ea4dbc2a4d66b2437680546ecd6415651
Corey N <coreynotice@yahoo.com>
2023-04-21 11:29:15 -0400

    Create Rating and Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,25 @@
+
+import Product from './componets/Product';
 import data from './data';
 import './index.css';
-import react from 'react';
+
 function App() {
   return (
     <div className="grid-container">
     <header className="row">
         <div>
-            <a className="brand" href="index.html">amazona</a>
+            <a className="brand" href="/index">amazona</a>
         </div>
     <div>
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>       
+        <a href="/cart">Cart</a>
+        <a href="/signin">Sign In</a>       
      </div>
     </header>
     <main>
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
+          <Product key={product._id}  product={product}/>
             ))}
           </div>
         </div>
